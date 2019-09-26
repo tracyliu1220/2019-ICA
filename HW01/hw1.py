9f5ced678e26ee2522103b8de7271da76f6b2826
Tracy Liu <tracyliu.cs06@nctu.edu.tw>
2019-09-26 23:08:55 +0800

    HW01

---
+++ HW01/hw1.py
@@ -0,0 +1,46 @@
+import math
+
+# 1(a)
+numbers = []
+
+# 1(b)
+sumup = 0
+
+# 3
+animals = {}
+
+rf = 'assn1_input.txt'
+af = 'animals.out'
+ff = 'fixed.out'
+
+with open(rf) as input_file, open(ff, 'w+') as fixed_file, open(af, 'w+') as animals_file:
+    data = input_file.readlines()
+
+    for line in data:
+
+        # 2
+        line.replace('zerba', 'zebra')
+        fixed_file.write(line)
+
+        line = line.strip().split(';')
+
+        # 1
+        if len(line) == 1 and line[0].isnumeric():
+            numbers.append(line[0])
+            sumup += int(line[0])
+
+        # 3
+        else:
+            for word in line:
+                if animals.get(word):
+                    animals[word] += 1
+                else:
+                    animals[word] = 1
+
+    # 1
+    sumup = math.sqrt(sumup)
+    animals_file.write(str(numbers) + '\n')
+    animals_file.write('%2e' % sumup + '\n')
+
+    # 3
+    animals_file.write(str(animals) + '\n')
